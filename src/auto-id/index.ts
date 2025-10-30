abf2426e82e7b7a1ea2e5a349d7561cdc5e6de67
Nakano as a Service <nakanoasaservice@gmail.com>
2025-10-30 10:01:21 +0900

    refactor: auto-idにテスト追加

--- src/auto-id/index.ts
+++ src/auto-id/index.ts
@@ -1,22 +1,21 @@
+const CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+// `CHARS.length` の倍数で最大の値
+const MAX_MULTIPLE = Math.floor(256 / CHARS.length) * CHARS.length
+const TARGET_LENGTH = 20
+
 /**
  * Firestoreが自動生成するものと同じ20文字のランダムなIDを生成する
  * @see https://github.com/firebase/firebase-js-sdk/blob/6e0e303173c93646c07b9138c7bed8749b514e8f/packages/firestore/src/util/misc.ts#L34
  * @returns ランダムなID
  */
 export function autoId(): string {
-  // 英数字
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-  // `char.length` の倍数で最大の値
-  const maxMultiple = Math.floor(256 / chars.length) * chars.length
-
   let autoId = ""
-  const targetLength = 20
-  while (autoId.length < targetLength) {
+  while (autoId.length < TARGET_LENGTH) {
     const bytes = crypto.getRandomValues(new Uint8Array(40))
     bytes.forEach((byte) => {
-      // [0, maxMultiple) の値のみを受け取ることで、`chars` に均等にマッピングできるようにする
-      if (autoId.length < targetLength && byte < maxMultiple) {
-        autoId += chars.charAt(byte % chars.length)
+      // [0, MAX_MULTIPLE) の値のみを受け取ることで、`CHARS` に均等にマッピングできるようにする
+      if (autoId.length < TARGET_LENGTH && byte < MAX_MULTIPLE) {
+        autoId += CHARS.charAt(byte % CHARS.length)
       }
     })
   }
